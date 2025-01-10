b93eaa6d75d9b7b2b8befdd8991e34b8f58f925d
pooja2443 <pooja.p@solutelabs.com>
2025-01-10 07:39:49 +0000

    text and view components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import React,{Component} from "react";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", justifyContent: "space-evenly", alignItems: "center", flexDirection : "row",flexWrap: "wrap" }}>
-      <View style={{ height: 200, width: 200, backgroundColor: "lightblue", marginBottom: 10 }}></View>
-      <View style={{ height: 200, width: 200, backgroundColor: "lightgreen", marginBottom: 10}}></View>
-      <View style={{ height: 200, width: 200, backgroundColor: "lightyellow", marginBottom: 10}}></View>
-      <View style={{ height: 200, width: 200, backgroundColor: "pink" }}></View>
+    <View 
+      style={{ flex: 1, backgroundColor: "plum", justifyContent: "space-evenly", alignItems: "center", flexDirection : "row",flexWrap: "wrap" }}>
+      <View style={{ height: 400, width: 400, backgroundColor: "lightblue", marginBottom: 10,justifyContent: "center" ,alignItems: "center"}}>
+          <View style={{ height: 300, width: 300, backgroundColor: "lightgreen", marginBottom: 10,justifyContent: "center" ,alignItems: "center",padding : 20}}>
+            <Text style = {{alignItems : "center", textAlignVertical : "center", color : "blue",fontWeight : "bold"}}>LEARNING CORE-COMPONENETS OF REACT NATIVE</Text>
+          </View>
+        
+      </View>
+      
+     
     </View>
   );
 }
@@ -16,77 +21,3 @@ export default function App() {
 
 
 
-// import { Image, StyleSheet, Platform } from 'react-native';
-
-// import { HelloWave } from '@/components/HelloWave';
-// import ParallaxScrollView from '@/components/ParallaxScrollView';
-// import { ThemedText } from '@/components/ThemedText';
-// import { ThemedView } from '@/components/ThemedView';
-
-// export default function HomeScreen() {
-//   return (
-//     <ParallaxScrollView
-//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-//       headerImage={
-//         <Image
-//           source={require('@/assets/images/partial-react-logo.png')}
-//           style={styles.reactLogo}
-//         />
-//       }>
-//       <ThemedView style={styles.titleContainer}>
-//         <ThemedText type="title">Welcome!</ThemedText>
-//         <HelloWave />
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         <ThemedText>
-//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-//           Press{' '}
-//           <ThemedText type="defaultSemiBold">
-//             {Platform.select({
-//               ios: 'cmd + d',
-//               android: 'cmd + m',
-//               web: 'F12'
-//             })}
-//           </ThemedText>{' '}
-//           to open developer tools.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//         <ThemedText>
-//           Tap the Explore tab to learn more about what's included in this starter app.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         <ThemedText>
-//           When you're ready, run{' '}
-//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         </ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
